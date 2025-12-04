c1f6ad3dd3fe4d0688bf8fb2044c3c3192a0db93
Rameen Khan <rameenk181@gmail.com>
2025-12-04 23:52:26 +0500

    add Prayer Settings From

--- src/components/SelectLocation.tsx
+++ src/components/SelectLocation.tsx
@@ -1,10 +1,18 @@
 import { Country, State } from "country-state-city"
 import { Select, SelectItem, SelectTrigger, SelectContent, SelectValue } from "@/components/ui/select"
 import { useEffect, useState } from "react"
+import type { IState } from 'country-state-city'
+import { calculationMethods } from "./data/constants"
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
+import { Label } from "./ui/label"
+import { Switch } from "./ui/switch"
+
+
+
 
 export const SelectLocation = () => {
-    const [selectedCountry, setSelectedCountry] = useState("")
-    const [allStates, setAllStates] = useState("")
+    const [selectedCountry, setSelectedCountry] = useState("x")
+    const [allCities, setAllCities] = useState<IState[]>([])
     const [selectedState, setSelectedState] = useState("")
 
     const countries = Country.getAllCountries()
@@ -12,14 +20,16 @@ export const SelectLocation = () => {
     useEffect(() => {
         const states = State.getStatesOfCountry(selectedCountry)
 
-        // setAllStates(states!)
+        setAllCities(states)
         console.log(selectedCountry)
     }, [selectedCountry])
 
     return (
         <div>
             <h3>Prayer Time Settings</h3>
-            <Select onValueChange={value => setSelectedCountry(value)}>
+
+            //country
+            <Select onValueChange={value => setSelectedCountry(value)} required>
                 <SelectTrigger className="w-[280px]">
                     <SelectValue placeholder="Select Your Country" />
                 </SelectTrigger>
@@ -30,6 +40,48 @@ export const SelectLocation = () => {
                     )}
                 </SelectContent>
             </Select>
+
+            //city
+            <Select disabled={!selectedCountry && !allCities}>
+                <SelectTrigger>
+                    <SelectValue placeholder="Select Your City" />
+                </SelectTrigger>
+                <SelectContent >
+                    {allCities.length === 0 ?
+                        <SelectItem value={selectedCountry}> {selectedCountry} </SelectItem>
+                        : allCities.map(city => <SelectItem key={city.isoCode} value={city.name}>
+                            {city.name}
+                        </SelectItem>)}
+                </SelectContent>
+            </Select>
+
+            //prayer calculation method
+            <Select defaultValue={"Muslim World League"}>
+                <SelectTrigger>
+                    <SelectValue placeholder="Muslim World League" />
+                </SelectTrigger>
+                <SelectContent>
+                    {calculationMethods.map((method, index) => <SelectItem key={index} value={index.toString()}> {method}</SelectItem>)}
+                </SelectContent>
+            </Select>
+
+            <RadioGroup className="flex gap-2">
+                <RadioGroupItem value="0" id="0" />
+                <Label htmlFor="0">Shafi</Label>
+                <RadioGroupItem value="1" id="1" />
+                <Label htmlFor="1">Hanafi (Later Asr)</Label>
+            </RadioGroup>
+
+            //midnightMode switch - add tooltip
+            <Switch id="midnightMode" />
+            <Label htmlFor="midnightMode">Midnight Mode</Label>
+
+            //tune - adjust prayer time
+
+            {/* To Do: Add tooltip*/}
+
+            {/* To Do: Add reminder Type */}
         </div>
     )
 }
+
